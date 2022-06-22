de692c48b854c66400722892435952a64bb551bb
mradul098 <64240676+mradul098@users.noreply.github.com>
2022-06-22 20:20:25 +0530

    Create Search In 2d Matrix.py

---
+++ Search In 2d Matrix.py
@@ -0,0 +1,19 @@
+from os import *
+from sys import *
+from collections import *
+from math import *
+
+def findTargetInMatrix(matrix, m, n, target):
+    if len(matrix) is 0:
+        return False
+    row = 0
+    col = len(matrix[0]) - 1
+    while row < len(matrix) and col >= 0:
+        if matrix[row][col] > target:
+            col -= 1
+        elif matrix[row][col] < target:
+            row += 1
+        else:
+            return True
+    return False
+	
